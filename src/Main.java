8b2854eecffd0118ae9513fdf793060449a26aa9
Alone_Ujju <ujjwalsonawat7@gmail.com>
2023-12-18 17:42:34 +0530

    Added Chnages

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ class Main {
         System.out.println("Choose an operator: +, -, *, or /");
         operator = input.next().charAt(0);
         
-        if (operator == '+') {
+        if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
             System.out.println("Enter first number");
             number1 = input.nextDouble();
 
@@ -43,109 +43,7 @@ class Main {
             }
 
             input.close();
-        } else if (operator == '-') {
-            System.out.println("Enter first number");
-            number1 = input.nextDouble();
-
-            System.out.println("Enter second number");
-            number2 = input.nextDouble();
-
-            switch (operator) {
-
-                case '+':
-                    result = number1 + number2;
-                    System.out.println(number1 + " + " + number2 + " = " + result);
-                    break;
-
-                cas:
-                    result = number1 - number2;
-                    System.out.println(number1 + " - " + number2 + " = " + result);
-                    break;
-                case '*':
-                    result = number1 * number2;
-                    System.out.println(number1 + " * " + number2 + " = " + result);
-                    break;
-
-                case '/':
-                    result = number1 / number2;
-                    System.out.println(number1 + " / " + number2 + " = " + result);
-                    break;
-
-                default:
-                    System.out.println("Invalid operator!");
-                    break;
-            }
-
-            input.close();
-        } else if (operator == '*') {
-            System.out.println("Enter first number");
-            number1 = input.nextDouble();
-
-            System.out.println("Enter second number");
-            number2 = input.nextDouble();
-
-            switch (operator) {
-
-                case '+':
-                    result = number1 + number2;
-                    System.out.println(number1 + " + " + number2 + " = " + result);
-                    break;
-
-                case '-':
-                    result = number1 - number2;
-                    System.out.println(number1 + " - " + number2 + " = " + result);
-                    break;
-                case '*':
-                    result = number1 * number2;
-                    System.out.println(number1 + " * " + number2 + " = " + result);
-                    break;
-
-                case '/':
-                    result = number1 / number2;
-                    System.out.println(number1 + " / " + number2 + " = " + result);
-                    break;
-
-                default:
-                    System.out.println("Invalid operator!");
-                    break;
-            }
-
-            input.close();
-        } else if (operator == "/") {
-            System.out.println("Enter first number");
-            number1 = input.nextDouble();
-
-            System.out.println("Enter second number");
-            number2 = input.nextDouble();
-
-            switch (operator) {
-
-                case '+':
-                    result = number1 + number2;
-                    System.out.println(number1 + " + " + number2 + " = " + result);
-                    break;
-
-                case '-':
-                    result = number1 - number2;
-                    System.out.println(number1 + " - " + number2 + " = " + result);
-                    break;
-                case '*':
-                    result = number1 * number2;
-                    System.out.println(number1 + " * " + number2 + " = " + result);
-                    break;
-
-                case '/':
-                    result = number1 / number2;
-                    System.out.println(number1 + " / " + number2 + " = " + result);
-                    break;
-
-                default:
-                    System.out.println("Invalid operator!");
-                    break;
-            }
-
-            input.close();
-        }
+        } 
 
 
     }
